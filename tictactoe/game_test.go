632897e06d4e2a8a8d9136da0c65de42b985dff4
Jamie-McCaw <psiral@yahoo.com>
2013-04-13 09:41:17 -0400

    removed a test accidently pushed with README changes

--- tictactoe/game_test.go
+++ tictactoe/game_test.go
@@ -1,18 +1,3 @@
 package tictactoe
 
 import ()
-
-func TestRunGame(t *testing.T) {
-	read, write, _ := os.Pipe()
-	oldStdin := os.Stdin
-	os.Stdin = read
-	defer func() {
-		os.Stdin = oldStdin
-	}()
-	testString := "6"
-	write.WriteString(testString)
-	write.Close()
-	t.Log("Start the PlayersTurn and submit good value that gets placed on the board")
-	PlayerTurn()
-	assert.Equal(t, board, OneMoveTestBoard())
-}
